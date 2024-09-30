efc81cbc61aabb6a9bf75759b4c6a05de3072d5e
Saurabh yadav <938saurabh@gmail.com>
2024-09-30 10:07:43 +0530

    Update chai.jsx

--- 01vitereact/src/chai.jsx
+++ 01vitereact/src/chai.jsx
@@ -1,4 +1,4 @@
-function Chai(){
+function Chai(){                        // function name should be in capital otherwise vite will not render and with basic create-react-app it doesn't matter.
     return(
         <h3>chai is ready</h3>
     )
