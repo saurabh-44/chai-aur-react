17a21c0a46c6238ea489aca2bc13f33c78f1aaae
Saurabh yadav <938saurabh@gmail.com>
2024-09-30 09:37:30 +0530

    Update index.js

--- 01basicreact/src/index.js
+++ 01basicreact/src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react';                   // Core foundational library which works on to take references 
+import ReactDOM from 'react-dom/client';     // It is the implementation on web
 
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));  // it makes self dom which is virtual dom and comapares with the main dom
 root.render(
  
     <App />
