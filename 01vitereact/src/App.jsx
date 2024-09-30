315502bfdadfd31a0b3c6e50d938a5b1d0013f36
Saurabh yadav <938saurabh@gmail.com>
2024-09-30 10:06:37 +0530

    Update App.jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const username = "chai aur code"
 
   return (
-    <>
+    <>                                // because of jsx we can return only one element but for that we can return multiple element under <></> 
     <Chai/>
     <h1>chai aur react {username}</h1>
     <p>test para</p>
