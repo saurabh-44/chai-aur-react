94be45b86393737bdd288f497226ebeac011d267
Saurabh yadav <938saurabh@gmail.com>
2024-10-01 10:11:28 +0530

    Update main.jsx

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react'                  // need to import React to write JSX (which is a syntax extension of JavaScript).
+import ReactDOM from 'react-dom/client'    // responsible for rendering your React components to the DOM
 import App from './App.jsx'
 
 function MyApp(){
@@ -19,15 +19,15 @@ function MyApp(){
 //     children: 'Click me to visit google'
 // }
 
-const anotherElement = (
+const anotherElement = (                        //creates a React element using JSX syntax. It's an anchor (<a>) tag linking to Google that opens in a new tab
     <a href="https://google.com" target='_blank'>Visit google</a>
 )
 
 const anotherUser = "chai aur react"
-const reactElement = React.createElement(
-    'a',
-    {href: 'https://google.com',target: '_blank' },
-    'click me to visit google',
+const reactElement = React.createElement(       // create React elements manually (without JSX). It takes three arguments
+    'a',                                        // The HTML tag (e.g., 'a' for an anchor).
+    {href: 'https://google.com',target: '_blank' }, // An object representing the properties/attributes 
+    'click me to visit google',                     // Children or content inside the element (text or other elements). 
     anotherElement
 )
 
