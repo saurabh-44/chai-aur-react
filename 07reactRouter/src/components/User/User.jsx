05d3babf451cc843296ad059daefb70d40fcbd4e
Saurabh yadav <938saurabh@gmail.com>
2024-10-02 17:31:56 +0530

    Update User.jsx

--- 07reactRouter/src/components/User/User.jsx
+++ 07reactRouter/src/components/User/User.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 
 function User() {
-    const {userid} = useParams()
+    const {userid} = useParams()       // at the time of routing we give the name userid so it will use same name here.
   return (
     <div className='bg-gray-600 text-white text-3xl p-4'>User: {userid}</div>
   )
