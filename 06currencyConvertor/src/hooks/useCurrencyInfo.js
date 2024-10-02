eea237cec74e7f01d10c4b7ff73dc231aefb466d
Saurabh yadav <938saurabh@gmail.com>
2024-10-02 12:06:06 +0530

    Update useCurrencyInfo.js

--- 06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -2,13 +2,14 @@ import {useEffect, useState} from "react"
 
 
 function useCurrencyInfo(currency){
-    const [data, setData] = useState({})
+    // we want to call api when someone use 
+    const [data, setData] = useState({})           // if value is not coming then then empty object will handle
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        .then((res) => res.json())
-        .then((res) => setData(res[currency]))
+        .then((res) => res.json())                 // response converted to json
+        .then((res) => setData(res[currency]))     // we are taking currency from the data and the key will be currency
         console.log(data);
-    }, [currency])
+    }, [currency])                                 // when currency will change then it will call again
     console.log(data);
     return data
 }
