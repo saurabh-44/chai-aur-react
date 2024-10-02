851857deea84f4a89f331031fb1a09e769afc875
Saurabh yadav <938saurabh@gmail.com>
2024-10-02 17:12:49 +0530

    Update Header.jsx

--- 07reactRouter/src/components/Header/Header.jsx
+++ 07reactRouter/src/components/Header/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
                     </Link>
                     <div className="flex items-center lg:order-2">
                         <Link
-                            to="#"
+                            to="#"       // to is used as href
                             className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Log in
@@ -35,7 +35,7 @@ export default function Header() {
                             <li>
                                 <NavLink
                                 to="/"
-                                    className={({isActive}) =>
+                                    className={({isActive}) =>    // is used to check Home is active or not if active then text color should be orange otherwise gray
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
