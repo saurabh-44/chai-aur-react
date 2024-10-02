64ebc801472fe4de8dcc1e04d9ca7af479ca43f2
Saurabh yadav <938saurabh@gmail.com>
2024-10-02 18:19:02 +0530

    Update UserContext.js

--- 08miniContext/src/context/UserContext.js
+++ 08miniContext/src/context/UserContext.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const UserContext = React.createContext()
+const UserContext = React.createContext()      
 
-export default UserContext;
+export default UserContext;              // UserContext is the provider which will wrap
 
